3d69f358df40368fc2c31514d98f2350e3790f4f
Nick Howson <nick.howson@gmail.com>
2014-07-29 15:17:45 +1000

    Initial commit

---
+++ ActiveDirectory/stale_host_cleanup.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+	Gets a list of all the stale computers (Not seen in 6+ months) timestamps them in the Description field and moves them to the appropriate AD Objects
+.DESCRIPTION
+	Uses the AD modle to get access to the Get-ADComputer & Move-ADObject cmdlets to allow the user to query a computer for its last seen date, then move it to an OU depending on its age. 
+	
+	This script is designed to be run from the task shedualer as a weekly task.
+.NOTES
+Author: Nick Howson <nick.howson@gmail.com>
+#>
+
+#Setup
+# More information on date and time fuctions see - http://technet.microsoft.com/en-us/library/ff730960.aspx
+$dateCurrent = Get-Date
