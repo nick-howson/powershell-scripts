0d2fe506f049e64a8140b92d0a4baa71c669430d
Nick Howson <nick.howson@gmail.com>
2014-07-30 12:00:38 +1000

    Basic CSV output
    Added basic CSV output for 6, 12 & 18 months stale computer lists.

--- ActiveDirectory/stale_host_cleanup.ps1
+++ ActiveDirectory/stale_host_cleanup.ps1
@@ -10,11 +10,30 @@ Author: Nick Howson <nick.howson@gmail.com>
 #>
 
 #Setup
+# Import the AD module
+Import-Module ActiveDirectory
+$output_file_name		= "staleComputers"
 # More information on date and time fuctions see - http://technet.microsoft.com/en-us/library/ff730960.aspx
 $dateCurrent = Get-Date
+$date6 = $dateCurrent.AddMonths(-6)
+$date12 = $dateCurrent.AddMonths(-12)
+$date18 = $dateCurrent.AddMonths(-18)
 
-<# quick string date to DateTime converter
-$string = "19/03/2010 14:11"
-$dateformat = "%d/MM/yyyy %H:mm"
-[datetime]::ParseExact($string,$dateformat,$null)
-#>
+################################
+# Generate CSV reports section #
+################################
+# Get all AD computers with lastLogonTimestamp of 6 months or older
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $date6} -Properties LastLogonTimeStamp |
+# Output hostname and lastLogonTimestamp into CSV
+select-object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} | export-csv $output_file_name"-6months.csv" -notypeinformation
+# Get all AD computers with lastLogonTimestamp of 6 months or older
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $date12} -Properties LastLogonTimeStamp |
+# Output hostname and lastLogonTimestamp into CSV
+select-object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} | export-csv $output_file_name"-12months.csv" -notypeinformation
+# Get all AD computers with lastLogonTimestamp of 6 months or older
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $date18} -Properties LastLogonTimeStamp |
+# Output hostname and lastLogonTimestamp into CSV
+select-object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} | export-csv $output_file_name"-18months.csv" -notypeinformation
+# press any key to continue prompt mimicks the old pause from batch files
+Write-Host "Press any key to continue ..."
+$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
